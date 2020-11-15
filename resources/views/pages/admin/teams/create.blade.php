56fb862f11e5d84196deddf745bc84024b33d357
akunbeben <akunbeben@gmail.com>
2020-11-15 13:37:13 +0800

    update View

--- resources/views/pages/admin/teams/create.blade.php
+++ resources/views/pages/admin/teams/create.blade.php
@@ -23,29 +23,31 @@
                         <div class="card-header">
                             <h4>Team registration form</h4>
                         </div>
-                        <div class="card-body">
-                            <div class="form-group">
-                                <label for="name">Team name</label>
-                                <input type="text" name="name" id="name" class="form-control">
+                        <form action="" method="post">
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="name">Team name</label>
+                                    <input type="text" name="name" id="name" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Team description</label>
+                                    <textarea name="description" id="description" class="form-control"></textarea>
+                                </div>
+                                <!-- <div class="form-group">
+                                    <label for="players">Players</label>
+                                    <select name="players" id="players" class="form-control select2" multiple>
+                                        <option value="Player 1">Player 1</option>
+                                        <option value="Player 2">Player 2</option>
+                                        <option value="Player 3">Player 3</option>
+                                        <option value="Player 4">Player 4</option>
+                                    </select>
+                                </div> -->
                             </div>
-                            <div class="form-group">
-                                <label for="description">Team description</label>
-                                <textarea name="description" id="description" class="form-control"></textarea>
+                            <div class="card-footer text-right pt-0">
+                                <button class="btn btn-secondary">Cancel</button>
+                                <button class="btn btn-primary">Save</button>
                             </div>
-                            <div class="form-group">
-                                <label for="players">Players</label>
-                                <select name="players" id="players" class="form-control select2" multiple>
-                                    <option value="Player 1">Player 1</option>
-                                    <option value="Player 2">Player 2</option>
-                                    <option value="Player 3">Player 3</option>
-                                    <option value="Player 4">Player 4</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="card-footer text-right pt-0">
-                            <button class="btn btn-secondary">Cancel</button>
-                            <button class="btn btn-primary">Save</button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
